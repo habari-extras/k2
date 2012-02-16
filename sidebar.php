bc164178572d064fd20089521d7ce6a2493c9990
Federico Panico <bpm230@gmail.com>
2012-02-16 14:21:51 -0300

    Removed search form from template and add habari's own search form on theme activation

--- sidebar.php
+++ sidebar.php
@@ -2,11 +2,6 @@
 <!-- sidebar -->
 <?php Plugins::act( 'theme_sidebar_top' ); ?>
 
-		<div id="search">
-		<h2><?php _e('Search', 'k2'); ?></h2>
-<?php $theme->display ('searchform' ); ?>
-		</div>
-
 <?php $about = Options::get( 'about' ); if( !empty( $about ) ) : ?>
 		<div class="sb-about">
 		<h2><?php _e('About', 'k2'); ?></h2>
